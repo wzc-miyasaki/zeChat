8d94c6b9dc2c35b1cb0cc33303da995462346148
wang zecheng <wang790734@gmail.com>
2024-12-07 15:42:03 +0800

    init project

---
+++ src/main/java/zec/ghibli/zechat/ZeChatApplication.java
@@ -0,0 +1,13 @@
+package zec.ghibli.zechat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZeChatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/zec/ghibli/zechat/ZeChatApplicationTests.java
@@ -0,0 +1,13 @@
+package zec.ghibli.zechat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
